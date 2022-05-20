3fbcca58bb91b7feb52ebaa79bf88f0b3e541c6e
yasindurgun <yasin.durgun52@gmail.com>
2022-05-20 15:17:26 +0300

    Add logger services

--- BookStore/Middlewares/CustomExceptionMiddleware.cs
+++ BookStore/Middlewares/CustomExceptionMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using BookStore.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using System;
@@ -11,9 +12,11 @@ namespace BookStore.Middlewares
     public class CustomExceptionMiddleware
     {
         private readonly RequestDelegate _next;
-        public CustomExceptionMiddleware(RequestDelegate next)
+        private readonly ILoggerService _loggerService;
+        public CustomExceptionMiddleware(RequestDelegate next, ILoggerService loggerService)
         {
             _next = next;
+            _loggerService = loggerService;
         }
 
         public async Task Invoke(HttpContext context)
@@ -22,11 +25,14 @@ namespace BookStore.Middlewares
             try
             {
                 string message = "[Request]  HTTP " + context.Request.Method + " - " + context.Request.Path;
-                Debug.WriteLine(message);
+                _loggerService.Write(message);
+
                 await _next(context);
                 watch.Stop();
+
                 message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + "in " + watch.Elapsed.TotalMilliseconds + " ms";
-                Debug.WriteLine(message);
+                _loggerService.Write(message);
+
             }
             catch (Exception ex)
             {
@@ -38,11 +44,11 @@ namespace BookStore.Middlewares
 
         private Task HandleException(HttpContext context, Exception ex, Stopwatch watch)
         {
-            string message = "[Error] HTTP " + context.Request.Method + " - " + context.Response.StatusCode + "Error Message" + ex.Message + " in " + watch.Elapsed.TotalMilliseconds;
-            Debug.WriteLine(message);
-
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            string message = "[Error] HTTP " + context.Request.Method + " - " + context.Response.StatusCode + "Error Message" + ex.Message + " in " + watch.Elapsed.TotalMilliseconds;
+            _loggerService.Write(message);
 
             var result = JsonConvert.SerializeObject(new { error = ex.Message }, Formatting.None);
             return context.Response.WriteAsync(result);
